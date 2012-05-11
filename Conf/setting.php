617c1c4f1711aeb52f4aaae5aabaef6eda4541ae
nana <nana@andrial.(none)>
2012-05-11 15:24:25 +0800

    aaaaaa

---
+++ Conf/setting.php
@@ -0,0 +1,185 @@
+<?php
+return array (
+  'db_type' => 'mysql',
+  'db_host' => 'localhost',
+  'db_name' => 'hezll_ppvod',
+  'db_user' => 'hezlldbuser',
+  'db_pwd' => 'KcZNGGNMEEYuAXP3',
+  'db_port' => '3306',
+  'db_prefix' => 'pp_',
+  'db_charset' => 'utf8',
+  'site_name' => 'ShotKi射客在线影视',
+  'site_path' => '',
+  'site_url' => 'http://www.shotki.com',
+  'default_theme' => 'qiyi',
+  'site_keywords' => '射客在线播放,电影推荐,电影排行,电视剧在线,电视剧下载',
+  'site_description' => '射客影视,电影推荐,电视剧下载,美剧在线,在线高清,最新,最全,最小众的在线电影',
+  'site_email' => 'shotki@sohu.com',
+  'site_copyright' => '射客在线影视提供的所有免费电影链接均通过搜索引擎采用非人工方式生成，免费电影内容均来自互联网。
+www.shotki.com自身不存储、控制、编辑修改第三方免费电影页面及内容。当权利人发现在射客在线影视中的链接所指向的快播电影内容侵犯其版权时，请权利人通知shotki@sohu.com，我们将依法采取措施移除相关快播电影内容。',
+  'site_tongji' => '<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push([\'_setAccount\', \'UA-5841026-1\']);
+  _gaq.push([\'_trackPageview\']);
+
+  (function() {
+    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<script src="http://s17.cnzz.com/stat.php?id=2697993&web_id=2697993&show=pic" language="JavaScript"></script>
+<script src="http://www.clicki.cn/boot/43282"></script>
+<script type="text/javascript">
+var _nct = _nct || [];
+
+(function() {
+var nc = document.createElement(\'script\'); nc.type = \'text/javascript\'; nc.async = true;
+nc.src = \'http://t.newscurve.com/nct.js\';
+var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(nc, s);
+})();
+</script>
+
+',
+  'site_icp' => '',
+  'site_by' => '-高清-在线-射客影视',
+  'url_model' => '2',
+  'url_html_suffix' => '.html',
+  'url_num_admin' => '15',
+  'admin_time_edit' => true,
+  'admin_time_limit' => '300',
+  'admin_order_type' => 'id',
+  'upload_path' => 'Upload',
+  'upload_style' => 'Y-m',
+  'upload_class' => 'jpg,gif,png,jpeg.bmp',
+  'upload_thumb' => false,
+  'upload_thumb_w' => '120',
+  'upload_thumb_h' => '140',
+  'upload_water' => false,
+  'upload_water_img' => 'Public/images/watermark.gif',
+  'upload_water_pct' => '75',
+  'upload_water_pos' => '5',
+  'upload_http' => false,
+  'upload_http_down' => '10',
+  'upload_http_prefix' => '',
+  'upload_ftp' => false,
+  'upload_ftp_host' => '',
+  'upload_ftp_user' => '',
+  'upload_ftp_pass' => '',
+  'upload_ftp_port' => '21',
+  'upload_ftp_dir' => '/',
+  'play_show' => '1',
+  'play_urllist' => '1',
+  'play_width' => '800',
+  'play_height' => '460',
+  'play_qvod' => 'http://www.shotki.com/Public/Qvod3.5.exe',
+  'play_gvod' => 'http://union.ff84.com/ads/vod/player.php?id=gvod',
+  'play_pvod' => 'http://union.ff84.com/ads/vod/player.php?id=pvod',
+  'play_web9' => 'http://union.ff84.com/ads/vod/player.php?id=web9',
+  'play_pipi' => 'http://union.ff84.com/ads/vod/player.php?id=pvod',
+  'play_playad' => 'http://www.shotki.com/Html/loading.html',
+  'play_language' => '粤语,台语,国语,韩语,日语,英语',
+  'play_area' => '香港,台湾,大陆,日韩,欧美,海外',
+  'play_year' => '2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000,1999',
+  'play_server' => 
+  array (
+    'down_a' => 'http://www.163.com/',
+    'down_b' => 'http://www.baidu.com/',
+  ),
+  'play_collect_time' => '5',
+  'play_collect_name' => '0',
+  'play_collect' => true,
+  'play_collect_content' => 
+  array (
+    0 => '<a href="http://www.shotki.com" target="_blank"><font color="red">射客在线影视</font></a>',
+    1 => '可以去豆瓣中查找相关详细影评',
+    2 => '时光网有相关电影发布介绍',
+    3 => '射客在线的影视均来自网络',
+    4 => '注意：电影播放可能需要下载快播插件，本站提供的是无广告纯净版，请放心下载',
+    5 => '射客在线拒绝弹窗，',
+    6 => '在线影视来源于网络，如果您发现片源与标题不符或者效果较差，请留言。',
+    7 => '没有更好只有最好的射客在线提供',
+    8 => '您可以通过分享推荐你开心，校内，QQ中的好友们观看本站电影',
+    9 => '别忘记给电影评分哦！',
+    10 => '可以留言嘛！',
+  ),
+  'tmpl_cache_on' => false,
+  'html_cache_on' => false,
+  'html_cache_time' => 0,
+  'html_read_type' => 0,
+  'html_file_suffix' => '.html',
+  'html_home_suffix' => '.html',
+  'html_cache_index' => '1',
+  'html_cache_list' => '1.5',
+  'html_cache_content' => '12',
+  'html_cache_play' => '12',
+  'html_cache_ajax' => '12',
+  '_htmls_' => 
+  array (
+    'home:index:index' => 
+    array (
+      0 => '{:action}',
+      1 => 3600,
+    ),
+    'home:vod:show' => 
+    array (
+      0 => '{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',
+      1 => 5400,
+    ),
+    'home:news:show' => 
+    array (
+      0 => '{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',
+      1 => 5400,
+    ),
+    'home:vod:read' => 
+    array (
+      0 => '{:module}_{:action}/{id|md5}',
+      1 => 43200,
+    ),
+    'home:news:read' => 
+    array (
+      0 => '{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',
+      1 => 43200,
+    ),
+    'home:vod:play' => 
+    array (
+      0 => '{:module}_{:action}/{id|md5}',
+      1 => 43200,
+    ),
+    'home:ajax:show' => 
+    array (
+      0 => '{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',
+      1 => 43200,
+    ),
+  ),
+  'play_list' => 
+  array (
+    '' => '',
+  ),
+  'url_html' => '0',
+  'url_dir_a' => '1',
+  'url_dir_b' => '1',
+  'url_time' => '2',
+  'url_number' => '100',
+  'url_vodlist' => 'vodlist/',
+  'url_voddata' => 'vod/',
+  'url_vodplay' => 'play/',
+  'url_newslist' => 'newslist/',
+  'url_newsdata' => 'news/',
+  'url_map' => 'map/',
+  'home_pagenum' => '3',
+  'home_pagego' => 'pagego',
+  'user_gbcm' => false,
+  'user_second' => '60',
+  'user_gbnum' => '8',
+  'user_cmnum' => '5',
+  'user_replace' => '她妈|它妈|他妈|你妈|去死|贱人',
+  'upload_ftp_del' => '0',
+  'play_baofeng' => '',
+  'play_bdhd' => '',
+  'play_second' => 0,
+);
+?>
